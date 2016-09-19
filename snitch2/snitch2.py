2c240985f885ab94afaf51304cb8ac99df971e70
Joel Colucci <joelcolucci@gmail.com>
2016-09-19 07:58:51 -0400

    refactor: check if uri visited before adding to queue

--- snitch2/snitch2.py
+++ snitch2/snitch2.py
@@ -22,32 +22,32 @@ def snitch(start_url, target_uri, max_crawl=1):
     while queue and pages_crawled < max_crawl:
         vertex = queue.popleft()
         
-        if vertex not in visited:
-            visited.add(vertex)
+        visited.add(vertex)
 
-            html_page = fetch_html(vertex)
-            links = parselink.get_links(vertex, html_page)
+        html_page = fetch_html(vertex)
+        links = parselink.get_links(vertex, html_page)
 
-            for link in links:
-                # Ensure we only crawl starting domain
-                if link['kind'] == 'internal':
-                    queue.append(link['uri'])
+        for link in links:
+            # Ensure we do not add pages previously crawled to queue
+            # and ensure we only crawl pages on starting domain
+            if link['uri'] not in visited and link['kind'] == 'internal':
+                queue.append(link['uri'])
 
-                if parselink.contains(link['uri'], target_uri) and link['type'] == 'absolute':
-                    # We don't consider relative links on target domain to be guilty only
-                    # hard coded absolutes
-                    results.append({
-                        "guilty_link": link,
-                        "target_uri": target_uri,
-                        "page_uri": vertex
-                    })
+            if parselink.contains(link['uri'], target_uri) and link['type'] == 'absolute':
+                # We don't consider relative links on target domain to be guilty only
+                # hard coded absolutes
+                results.append({
+                    "guilty_link": link,
+                    "target_uri": target_uri,
+                    "page_uri": vertex
+                })
 
-            pages_crawled += 1
+        pages_crawled += 1
 
     response = {
         "start_url": start_url,
         "target_uri": target_uri,
-        "pages_crawled": len(visited),
+        "pages_crawled": pages_crawled,
         "guilty_total": len(results),
         "guilty_results": results  
     }
@@ -62,4 +62,4 @@ def fetch_html(uri):
 
 
 if __name__ == '__main__':
-    print snitch('//joelcolucci.com', 'http://github.com', 2)
+    print snitch('//joelcolucci.com', 'github.com', 2)
