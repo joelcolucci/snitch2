837b0ae879423ebf0d52a36e02f83c02c33c064a
Joel Colucci <joelcolucci@gmail.com>
2016-09-19 08:01:02 -0400

    tests: remove deprecated tests

--- tests/test_snitch2.py
+++ tests/test_snitch2.py
@@ -16,33 +16,6 @@ class Snitch2TestCase(TestCase):
     def tearDown(self):
         pass
 
-    # def test_contains_returns_true(self):
-    #     str1 = 'google.com/maps'
-    #     str2 = 'google.com'
-
-    #     self.assertTrue(snitch2.contains(str1, str2))
-
-    # def test_contains_fail_returns_false(self):
-    #     str1 = 'joelcolucci.com'
-    #     str2 = 'google.com'
-
-    #     self.assertFalse(snitch2.contains(str1, str2))
-
-    # def test_contains_handles_none(self):
-    #     str1 = None
-    #     str2 = 'google.com'
-
-    #     self.assertFalse(snitch2.contains(str1, str2))
-
-    #     str1 = 'google.com'
-    #     str2 = None
-
-    #     self.assertFalse(snitch2.contains(str1, str2))
-
-    def test_fetch_html(self):
-        pass
-
-
 
 if __name__ == '__main__':
     main()
